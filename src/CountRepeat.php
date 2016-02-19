7edc56b1df5b4462a2cd0f3cef8d8677a6bcdd21
erik <tolentino3575@gmail.com>
2016-02-19 09:43:37 -0800

    Pass fourth spec, catch syntax error

--- src/CountRepeat.php
+++ src/CountRepeat.php
@@ -24,9 +24,16 @@
 
         function countWord($input_string, $input_word)
         {
-            
-        }
+            $string_words = explode(" ", $input_string);
+            $word_counter = 0;
 
+            foreach($string_words as $word) {
+                if ($word == $input_word) {
+                    $word_counter += 1;
+                }
+            }
+                    return $word_counter;
+        }
     }
 
 ?>
