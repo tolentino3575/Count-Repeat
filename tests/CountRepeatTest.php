bc3ed51f3ddfaba1784ff913c717d138e507c45b
erik <tolentino3575@gmail.com>
2016-02-19 08:52:25 -0800

    Get things working, write and pass first spec

---
+++ tests/CountRepeatTest.php
@@ -0,0 +1,34 @@
+<?php
+
+    require_once "src/CountRepeat.php";
+
+    class CountRepeatTest extends PHPUnit_Framework_TestCase
+    {
+        function test_input_word()
+        {
+        //Arrange
+        $test_CountRepeat = new CountRepeat;
+        $input = 'burger';
+
+        //Act
+        $result = $test_CountRepeat->singleWord($input);
+
+        //Assert
+        $this->assertEquals("burger", $result);
+        }
+
+        function test_input_multiple_words()
+        {
+        //Arrange
+        $test_CountRepeat = new CountRepeat;
+        $input = "delicious burger";
+
+        //Act
+        $result = $test_CountRepeat->multipleWords($input);
+
+        //Assert
+        $this->assertEquals("delicious burger", $result);
+        }
+    }
+
+?>
